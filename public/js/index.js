0586d368445f68fd0421557a2950e11e69a3b3ed
Hyeon <20310911+hshong9590@users.noreply.github.com>
2018-01-05 10:34:10 +0900

    Revert "Add event acknowledgements"
    This reverts commit cc206e2fd35ba74fa6a70a33c5879eec9d0b7466.

--- public/js/index.js
+++ public/js/index.js
@@ -11,10 +11,3 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
 });
-
-socket.emit('createMessage', {
-  from: 'Frank',
-  text: 'Hi'
-}, function (data) {
-  console.log('Got it', data);
-});
